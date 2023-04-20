0ece0f8ce126abb9b57d591e2161bf83315716da
abu jafar <abujafarshiddik123@gmail.com>
2023-04-20 14:26:24 +0600

    prctice DP

---
+++ fibo.cpp
@@ -0,0 +1,24 @@
+/**
+ *author:Abu Jafar Shiddik
+ *created:19-04-2023(15:51:32)
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+long long arr[101];
+long long fibo(int n)
+{
+    if (n <= 2)
+        return 1;
+    if (arr[n] != 0)
+    {
+        return arr[n];
+    }
+    arr[n] = fibo(n - 1) + fibo(n - 2);
+    return arr[n];
+}
+int main()
+{
+    cout << (fibo(5)) << "\n";
+    cout << (fibo(50)) << "\n";
+    return 0;
+}
